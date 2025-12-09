20ef3e748e887e780e0ea57a214941533104bc4f
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-12-09 19:59:19 +0100

    fred i musica

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+while (true) {
+    if (input.temperature() > 22) {
+        basic.showString("calor")
+    } else {
+        basic.showString("fred")
+    }
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
+    } else {
+        if (input.lightLevel() < 50) {
+            music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.InBackground)
+        }
+    }
+}
